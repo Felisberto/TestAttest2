3b72452d96d9d1f9888190503687f9b3c6a8bd46
feldrig <felisbertornascimento@itau-unibanco.com.br>
2019-08-22 18:00:32 -0300

    advances

--- TestAttest/app/src/main/java/com/example/testattest/MainActivity.kt
+++ TestAttest/app/src/main/java/com/example/testattest/MainActivity.kt
@@ -3,6 +3,9 @@ package com.example.testattest
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
+import android.view.View
+import android.widget.CheckBox
+import android.widget.Toast
 import com.example.testattest.adapter.RcAdapter
 import com.example.testattest.model.ModelTest
 import kotlinx.android.synthetic.main.activity_main.*
@@ -18,6 +21,38 @@ class MainActivity : AppCompatActivity() {
         rvTest.run {
             layoutManager = LinearLayoutManager(this@MainActivity)
             adapter = thisAdapter
+        }
+    }
+
+    fun onCheckboxClicked(view: View) {
+        if (view is CheckBox) {
+            val checked: Boolean = view.isChecked
+
+            when (view.id) {
+                R.id.checkbox_meat -> {
+                    if (checked) {
+                        // Put some meat on the sandwich
+
+                        Toast.makeText(applicationContext, "More Meat", Toast.LENGTH_LONG).show()
+
+                    } else {
+                        // Remove the meat
+                        Toast.makeText(applicationContext, "No Meat", Toast.LENGTH_LONG).show()
+
+                    }
+                }
+                R.id.checkbox_cheese -> {
+                    if (checked) {
+                        // Cheese me
+                        Toast.makeText(applicationContext, "Queso", Toast.LENGTH_LONG).show()
+
+                    } else {
+                        // I'm lactose intolerant
+                        Toast.makeText(applicationContext, "Vegan", Toast.LENGTH_LONG).show()
+
+                    }
+                }
+            }
         }
     }
 }
